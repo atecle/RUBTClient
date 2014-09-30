89ccbee341cef6827395d9174544cf9e073402cc
Adam <Tecle@adams-mbp.home>
2014-09-30 01:35:56 -0400

    successfully contacted tracker

--- src/Tracker.java
+++ src/Tracker.java
@@ -9,12 +9,24 @@ import java.util.Random;
 public class Tracker {
 
 	private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	private static String info_hash;
+	private static String peer_id;
+	private static int port;
+	private static int uploaded;
+	private static int downloaded;
+	private static int left;
+	
+	
 	private TorrentInfo torrent;
 
 
 	public Tracker(TorrentInfo torr) {
 		this.torrent = torr;
+		this.peer_id = randomAlphaNumeric();
+		this.uploaded = 0;
+		this.downloaded = 0;
+		this.port = 6881;
 	}
 
 	public String[] getPeerList() {
@@ -26,18 +38,20 @@ public class Tracker {
 
 	public String sendGet() throws Exception {
 
-		URL trackerURL = torrent.announce_url;
-		System.out.println(randomAlphaNumeric());
+		
+		URL trackerURL = constructURL(this.torrent);
+		trackerURL.openConnection();
+		
 		HttpURLConnection conn = (HttpURLConnection) trackerURL.openConnection();
 
 		conn.setRequestMethod("GET");
 		conn.connect();
-		System.out.println(conn.getResponseCode());
+		System.out.println(conn.getResponseCode()); 
 		return null;
 	}
+	
 
-
-	public static String randomAlphaNumeric() {
+	private static String randomAlphaNumeric() {
 		StringBuilder builder = new StringBuilder();
 		int count = 20;
 		while (count-- != 0) {
@@ -45,5 +59,25 @@ public class Tracker {
 			builder.append(ALPHA_NUMERIC_STRING.charAt(character));
 		}
 		return builder.toString();
+	}
+	
+	private URL constructURL(TorrentInfo torrent) throws MalformedURLException {
+		
+		String base_url = torrent.announce_url.toString();
+		String info_hash = bytesToHex(torrent.info_hash.array());
+		String get_url = base_url + "?" + "info_hash=" + info_hash + "?" + "peer_id=" + peer_id + "?" + "port=" + port + "?" + "downloaded=0" +"left=0";
+	//	System.out.println(get_url);
+		URL url = new URL(get_url);
+		return url;
+	}
+	
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
 	}
 }
